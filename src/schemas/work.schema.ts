24ab7b06df7eadcbbb3125cbbf932a9718709809
Luis Tapia <133921246+Tapia-GJ@users.noreply.github.com>
2026-04-14 17:33:07 -0500

    feat: add esquema de validación work

---
+++ src/schemas/work.schema.ts
@@ -0,0 +1,11 @@
+import { z } from "zod";
+
+export const createWorkSchema = z.object({
+  title: z.string().min(1, "El título es obligatorio"),
+  description: z.string().min(1, "La descripción es obligatoria"),
+  coverUrl: z.string().url("Debe ser una URL válida").optional(),
+  publishYear: z.number().int().optional(),
+  authorId: z.number().int().positive("El ID del autor es obligatorio"),
+});
+
+export const updateWorkSchema = createWorkSchema.partial();
